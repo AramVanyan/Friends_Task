6cc7cfa32857a4efb625e04eefc070f3a7ccd49c
Student <Student@EPAMYERW0066.moscow.epam.com>
2020-01-27 18:15:03 +0400

    some changes

--- src/FriendsCounter.java
+++ src/FriendsCounter.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class FriendsCounter {
     public static int countFriends(String[] listOfRelationships,String name) {
         int currentIndex = -1;
-        String[] listOfNames = null;
+        String[] listOfNames = new String[listOfRelationships.length];
         HashSet<String> setOfNonRepeatingNames = new HashSet<>();
-        HashSet<String> setOfFriends = null;
+        HashSet<String> setOfFriends = new HashSet<>();
         HashMap<String,String[]> mapOfRelationships = new HashMap<>();
-        ArrayList<String> arrayListOfNonRepeatingList = null;
 
 
         for (int i = 0; i < listOfRelationships.length; i++) {
@@ -17,13 +16,7 @@ public class FriendsCounter {
             }
         }
 
-        for (int i = 0; i < listOfNames.length; i++) {
-            setOfNonRepeatingNames.add(listOfNames[i]);
-        }
-        for (Iterator<String> it = setOfNonRepeatingNames.iterator();it.hasNext();) {
-            arrayListOfNonRepeatingList.add(it.next());
-        }
-
+        setOfNonRepeatingNames.addAll(Arrays.asList(listOfNames));
         for (int i = 0; i < setOfNonRepeatingNames.size(); i++) {
             String currentName;
             String[] friends = new String[setOfNonRepeatingNames.size()-1];
@@ -41,10 +34,9 @@ public class FriendsCounter {
             mapOfRelationships.put(currentName,friends);
         }
         String[] tempFriends = mapOfRelationships.get(name);
-        for (String friend:tempFriends) {
-            setOfFriends.add(friend);
-        }
+        setOfFriends.addAll(Arrays.asList(tempFriends));
         Iterator it = mapOfRelationships.entrySet().iterator();
+
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
             if (pair.getKey() == name) {
@@ -52,7 +44,7 @@ public class FriendsCounter {
             }
             tempFriends = (String[]) pair.getValue();
             for (String friend:tempFriends) {
-                if (friend == name ) {
+                if (friend.equals(name)) {
                     continue;
                 }
                 setOfFriends.add(friend);
