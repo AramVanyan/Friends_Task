c1ad3a66d146b99f0cd6ec38f8e23d0445884552
Student <Student@EPAMYERW0066.moscow.epam.com>
2020-01-27 17:59:59 +0400

    middle state of app

--- src/FriendsCounter.java
+++ src/FriendsCounter.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 public class FriendsCounter {
-    public static void countFriends(String[] listOfRelationships,String name) {
+    public static int countFriends(String[] listOfRelationships,String name) {
         int currentIndex = -1;
         String[] listOfNames = null;
         HashSet<String> setOfNonRepeatingNames = new HashSet<>();
+        HashSet<String> setOfFriends = null;
         HashMap<String,String[]> mapOfRelationships = new HashMap<>();
         ArrayList<String> arrayListOfNonRepeatingList = null;
 
@@ -28,18 +26,39 @@ public class FriendsCounter {
 
         for (int i = 0; i < setOfNonRepeatingNames.size(); i++) {
             String currentName;
-            String nextName;
-            int currentIndex1 = -1;
-            String[] tempArrayOfNames;
+            String[] friends = new String[setOfNonRepeatingNames.size()-1];
+            int currentIndexOfFriends = -1;
             Iterator<String> it = setOfNonRepeatingNames.iterator();
+            currentName = it.next().toString();
             for (int j = 0; j < listOfNames.length; j+=2) {
-                currentName = it.next().toString();
-
+                if (currentName.equals(listOfNames[j])) {
+                    friends[++currentIndexOfFriends] = listOfNames[j+1];
+                }
+                if (currentName.equals(listOfNames[j+1])) {
+                    friends[++currentIndexOfFriends] = listOfNames[j];
+                }
+            }
+            mapOfRelationships.put(currentName,friends);
+        }
+        String[] tempFriends = mapOfRelationships.get(name);
+        for (String friend:tempFriends) {
+            setOfFriends.add(friend);
+        }
+        Iterator it = mapOfRelationships.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (pair.getKey() == name) {
+                continue;
+            }
+            tempFriends = (String[]) pair.getValue();
+            for (String friend:tempFriends) {
+                if (friend == name ) {
+                    continue;
+                }
+                setOfFriends.add(friend);
             }
 
-
         }
-
-
+        return setOfFriends.size();
     }
 }
